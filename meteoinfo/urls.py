bb3112782aa512c16ae5a9e5d8c1720f455a9028
Oleksandr Kovalchuk <ol.kovalchuk.1989@gmail.com>
2026-08-05 14:26:20 +0000

    Displaying meteodata for all time

--- meteoinfo/urls.py
+++ meteoinfo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import all_readings, home
 
 urlpatterns = [
     path("", home, name="home"),
+    path("all/", all_readings, name="all_readings"),
 ]
--- meteoinfo/views.py
+++ meteoinfo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 from api.models import Reading
 
@@ -11,5 +12,23 @@ def home(request):
         "home.html",
         {
             "reading": reading,
+        },
+    )
+
+
+def all_readings(request):
+    readings = Reading.objects.order_by("-created_at")
+
+    paginator = Paginator(readings, 50)
+
+    page_number = request.GET.get("page")
+
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        "all.html",
+        {
+            "page_obj": page_obj,
         },
     )
